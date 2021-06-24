19c923c32c6ac5b0bce4b443617c728d84a13b8f
jelenapetrovacki <jpetrovacki3@gmail.com>
2021-06-24 11:24:22 +0200

    Initial commit

---
+++ CadastralDataApp/src/main/java/dj/CadastralDataAppApplication.java
@@ -0,0 +1,13 @@
+package dj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastralDataAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastralDataAppApplication.class, args);
+	}
+
+}
---
+++ CadastralDataApp/src/test/java/dj/CadastralDataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastralDataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
